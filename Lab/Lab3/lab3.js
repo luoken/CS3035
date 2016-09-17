99eed42e41df5ef558f05967e2ff2f1dabc5708b
luoken <luozhirong@gmail.com>
2016-09-16 22:36:05 -0700

    add last name

--- Lab/Lab3/lab3.js
+++ Lab/Lab3/lab3.js
@@ -9,19 +9,19 @@ console.log(array.reduce(function(a,b){
 
 //problem 2
 var people = [
-	      {"name" : "Barbs", "year" : "1940"},
-	      {"name" : "Gary", "year" : "1935"},
-	      {"name" : "Gabriel", "year" : "1943"},
-	      {"name" : "Rudy", "year" : "1955"},
-	      {"name" : "Levin", "year" : "1967"},
-	      {"name" : "Mark", "year" : "1975"},
-	      {"name" : "Matthew", "year" : "1978"},
-	      {"name" : "Karen", "year" : "1994"},
-	      {"name" : "Ray", "year" : "1904"},
-	      {"name" : "Misty", "year" : "2000"},
-	      {"name" : "Ash", "year" : "2005"},
-	      {"name" : "Maggie", "year" : "1997"},
-	      {"name" : "Bart", "year" : "1991"},
+	      {"firstName" : "Barbs", "lastName": "Rodriguez", "year" : "1940"},
+	      {"firstName" : "Gary", "lastName": "Te", "year" : "1935"},
+	      {"firstName" : "Gabriel", "lastName" : "Sanchez", "year" : "1943"},
+	      {"firstName" : "Rudy", "lastName": "Gomez", "year" : "1955"},
+	      {"firstName" : "Levin", "lastName" : "Kim", "year" : "1967"},
+	      {"firstName" : "Mark", "lastName": "Yap", "year" : "1975"},
+	      {"firstName" : "Matthew", "lastName": "Johnson", "year" : "1978"},
+	      {"firstName" : "Karen", "lastName": "Chen", "year" : "1994"},
+	      {"firstName" : "Ray", "lastName": "Chan", "year" : "1904"},
+	      {"firstName" : "Misty", "lastName": "RedHead", "year" : "2000"},
+	      {"firstName" : "Ash", "lastName": "Ketchum", "year" : "2005"},
+	      {"firstName" : "Maggie", "lastName": "Simpson", "year" : "1997"},
+	      {"firstName" : "Bart", "lastName" : "Simpson", "year" : "1991"},
 	      ]
 
     people.forEach(function(temp){
