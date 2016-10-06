fe9a8f8b6029107646efe16e04ca996ae5bb37c5
luoken <luozhirong@gmail.com>
2016-10-06 11:51:02 -0700

    delete white spaces

--- Lab/Lab4/lab4.js
+++ Lab/Lab4/lab4.js
@@ -21,10 +21,9 @@ function inner(input){
 	console.log(e);
     }
 }
+inner("This class is awesome");
 
 //problem 2
-console.log();
-console.log("Problem 2:");
 function brokenError(message){
     this.message = message;
     this.stack = (new Error()).stack;
@@ -32,7 +31,6 @@ function brokenError(message){
 
 brokenError.prototype = Object.create(Error.prototype);
 brokenError.prototype.name = "brokenError";
-
 
 //problem 3
 console.log();
@@ -48,9 +46,27 @@ function outerWrapper(innerWrapper){
     }
 }
 
-
-
 var sentence1 = "this is sentence one";
 var sentence2 = "this is sentence two";
-var sentence3 = "this is sentence two";
+var sentence3 = "this is sentence three";
+
 console.log(outerWrapper(sentence1));
+
+var arrayOfStrings = [sentence1, sentence2, sentence3];
+//problem 4
+console.log();
+console.log("Problem 4");
+function test(arrayOfStrings){
+    if(outerWrapper(arrayOfStrings[0]) == true){
+	return console.log("Sentence 1: Passed");
+	}
+    if(outerWrapper(arrayOfStrings[1])){
+	return console.log("Sentence 2: Passed");
+	}
+    if(outerWrapper(arrayOfStrings[2])){
+	return console.log("Sentence 3: Passed");
+	}
+
+}
+
+test();
