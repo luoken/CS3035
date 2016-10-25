8421561638ac730cc7bb42f633ae4c5cc3bf9194
luoken <luozhirong@gmail.com>
2016-10-24 19:12:42 -0700

    finish problem 3

--- Lab/Lab6/lab6.js
+++ Lab/Lab6/lab6.js
@@ -1,25 +1,22 @@
 var fs = require("fs");
-var temp = fs.readFile("lab6.html", "utf8", function(error, text){
-    if(error)
-	throw error;
-    return console.log("The file contained:", text);    
-
-//    return text;
-
-});
-fs.readFile("lab6.html", function(error, buffer){
-    if(error)
-	throw error;
-//    console.log("The file contained", buffer.length, "bytes.", "The first byte is:", buffer[0]);
-});
+var file = "lab6.html";
+var holder;
 
 
-//temp;
+
+var fileToString = function(fileName){
+    var fileText;
+    fileText = fs.readFileSync(fileName, "utf8");
+    return fileText;
+}
+
+
+console.log(fileToString(file));
+
 var http = require("http");
-var server = http.createServer(function(request,response){
-    response.writeHead(200, {"Content-Type": "text/html"});
-    response.write(temp);
+var server = http.createServer(function(request, response){
+    response.writeHead(200, {"Content-Type": "text/html "});
+    response.write(fileToString(file));
     response.end();
 });
 server.listen(8000);
-
